518b6385bf38197a926e7144f45d049e742c2f11
Vladimir Proshin <VlaShin1986@gmail.com>
2022-12-03 20:12:06 +0200

    initial method

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    int[][] initialArray = new int[5][5];
+
+    public static void main(String[] args) {
+
+    }
+    public static void rotateArray(int[][] array, int degree){
+        for (int i = 0; i < array.length; i++) {
+
+        }
+        if(degree == 90){
+
+        }
+    }
+}
